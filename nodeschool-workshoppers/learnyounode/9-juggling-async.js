402706eecb78c32948536f6aa0f5aabfc5bd487f
gregalexsmith <gregalexsmith@gmail.com>
2017-02-07 20:04:35 -0500

    fix-typos

--- nodeschool-workshoppers/learnyounode/9-juggling-async.js
+++ nodeschool-workshoppers/learnyounode/9-juggling-async.js
@@ -9,8 +9,8 @@ var urls = process.argv.splice(2, process.argv.length - 2);
 var responses = [];
 var receivedCount = 0;
 
-// perform HTTP GET requres to a url at specified index
-// once data is received from the server, add that data to the results array at the correct location
+// perform HTTP GET request to a url at specified index
+// once data is received from the server, add that data to the results array at the correct locations
 // finally, check to see if all URLS have been received, if so print results
 var httpGetAndCheck = function(index) {
   http.get(urls[index], function(res) {
@@ -18,7 +18,7 @@ var httpGetAndCheck = function(index) {
         if (err) {
           return console.error(err)
         }
-        //all data recieved, add to responses array at correct location;
+        //all data received, add to responses array at correct location;
         responses[index] = buf.toString();
         receivedCount++;
         //check if all urls have been received
@@ -38,6 +38,6 @@ var printResult = function(result) {
 for (var i = 0; i < urls.length; i++) {
   //fill the response array to required length
   responses.push("");
-  //perform get request and check for sucess
+  //perform get request and check for success
   httpGetAndCheck(i);
 }
