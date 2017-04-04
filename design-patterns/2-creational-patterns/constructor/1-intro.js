fe3fd0aaf80823a93d6d39637da92547793eb3e7
gregalexsmith <gregalexsmith@gmail.com>
2017-04-03 22:58:57 -0400

    cleanup

--- design-patterns/2-creational-patterns/constructor/1-intro.js
+++ design-patterns/2-creational-patterns/constructor/1-intro.js
@@ -1,3 +1,13 @@
+// Constructor Pattern
+// used to create objects with their own object scope
+
+// Using the 'new' keyword:
+//  - creates a brand new object
+//  - links to an object prototype
+//  - Binds 'this' to the new object scope
+//  - Implicitly returns 'this'
+
+// EXAMPLE
 var Task = function(name) {
   this.name = name;
   this.completed = false;
